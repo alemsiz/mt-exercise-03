f6432ddfe600ecd2e8afe9e9c8c39f8244f45dff
alemsiz <alicesizer15@gmail.com>
2024-04-23 00:26:31 +0200

    Adding solutions to part 1

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -20,24 +20,24 @@ done
 
 # download a different interesting data set!
 
-mkdir -p $data/grimm
+mkdir -p $data/dante
 
-mkdir -p $data/grimm/raw
+mkdir -p $data/dante/raw
 
-wget https://www.gutenberg.org/files/52521/52521-0.txt
-mv 52521-0.txt $data/grimm/raw/tales.txt
+wget https://www.gutenberg.org/files/8800/8800-0.txt
+mv 8800-0.txt $data/dante/raw/divine_comedy.txt
 
 # preprocess slightly
 
-cat $data/grimm/raw/tales.txt | python $base/scripts/preprocess_raw.py > $data/grimm/raw/tales.cleaned.txt
+cat $data/dante/raw/divine_comedy.txt | python $base/scripts/preprocess_raw.py > $data/dante/raw/divine_comedy.cleaned.txt
 
 # tokenize, fix vocabulary upper bound
 
-cat $data/grimm/raw/tales.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 5000 --tokenize --lang "en" --sent-tokenize > \
-    $data/grimm/raw/tales.preprocessed.txt
+head -n 10133 $data/dante/raw/divine_comedy.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 5000 --tokenize --lang "en" > \
+    $data/dante/raw/divine_comedy.preprocessed.txt
 
 # split into train, valid and test
 
-head -n 440 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/valid.txt
-head -n 840 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/test.txt
-tail -n 3075 $data/grimm/raw/tales.preprocessed.txt | head -n 2955 > $data/grimm/train.txt
+head -n 1133 $data/dante/raw/divine_comedy.preprocessed.txt | tail -n 1000 > $data/dante/valid.txt
+head -n 2133 $data/dante/raw/divine_comedy.preprocessed.txt | tail -n 1000 > $data/dante/test.txt
+head -n 10133 $data/dante/raw/divine_comedy.preprocessed.txt | tail -n 8000 > $data/dante/train.txt
